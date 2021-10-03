603fd640fc1ddab0679532605c4548c50f7a64ad
PiKiP <solonko.1996@mail.ru>
2021-10-03 17:53:01 +0300

    homework1_1

---
+++ homework1_1.py
@@ -0,0 +1,20 @@
+"""
+Задание:
+
+name= "Василий"
+surname = "Иванов"
+age = 33
+height = 182
+
+year = 2021
+word = "ростом"
+word2 = "см"
+
+Ответ:
+birth_year = year - age
+print(birth_year)
+
+bio = name + " " + surname + " " + str(birth_year) + " " + word + " " + str(height) + " " + word2
+print(bio)
+
+"""
