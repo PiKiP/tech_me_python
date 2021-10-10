46eedcc4f219894245f02b783fadbeab4b7496b4
PiKiP <91568618+PiKiP@users.noreply.github.com>
2021-10-10 11:44:18 +0200

    #3

--- homework_3/#3.py
+++ homework_3/#3.py
@@ -8,7 +8,8 @@ m = 1
 while n < user_count:
     data = user_input.pop(0)
     a = "".join(data)
-    result = ("{}".format(m) + "." + "{}".format(a).center(10))
+    b = a[0:10]
+    result = ("{}".format(m) + "." + "{}".format(b).center(10))
     n+=1
     m+=1
     print(result)
